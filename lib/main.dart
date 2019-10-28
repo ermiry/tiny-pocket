241bb36eba52f2cdd416cd9e89fcda2e67dcf563
ermiry <stumpestro@ermiry.com>
2019-10-27 20:51:45 -0600

    Work on transaction display and format

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 import 'package:pocket/transaction.dart';
 
 void main () => runApp (TinyPocket ());
@@ -39,7 +41,49 @@ class HomePage extends StatelessWidget {
 						color: Colors.blue, 
 						child: Text ('Chart!')
 						),
-					Card (child: Text ('List of TX!'))
+					Column (
+						children: transactions.map ((tx) {
+							return Card (
+								child: Row (children: <Widget>[
+									Container (
+										margin: EdgeInsets.symmetric (vertical: 10, horizontal: 15),
+										decoration: BoxDecoration (
+											border: Border.all (
+												color: Colors.purple,
+												width: 2
+											)
+											),
+										padding: EdgeInsets.all(10),
+										child: Text (
+											'\$${tx.amount}', 
+											style: TextStyle (
+												fontWeight: FontWeight.bold,
+												fontSize: 20,
+												color: Colors.purple
+											)
+											)
+										),
+									Column (
+										crossAxisAlignment: CrossAxisAlignment.start,
+										children: <Widget>[
+											Text (
+												tx.title,
+												style: TextStyle (
+													fontSize: 18,
+													fontWeight: FontWeight.bold
+												),
+												),
+											Text (
+												DateFormat ().format (tx.date),
+												style: TextStyle (
+													color: Colors.grey
+												),
+												)
+									],)
+								],)
+							);
+						}).toList (),
+					)
 				]
 			)
 		)
