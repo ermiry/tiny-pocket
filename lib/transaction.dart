796bd6a98fd9f0219b37c32a08685478891ddf15
ermiry <stumpestro@ermiry.com>
2019-10-27 20:18:26 -0600

    Added transaction class blueprint

---
+++ lib/transaction.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+
+	String id;
+	String title;
+	double amount;
+	DateTime date;
+
+	Transaction ({
+		@required this.id, 
+		@required this.title, 
+		@required this.amount, 
+		@required this.date});
+
+}
