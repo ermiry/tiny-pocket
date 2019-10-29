12bd48bed981fa7c2be64784d70d51c3b92d080d
ermiry <stumpestro@ermiry.com>
2019-10-28 22:54:30 -0600

    Refactor transaction list component

--- lib/widgets/transactions/list.dart
+++ lib/widgets/transactions/list.dart
@@ -32,45 +32,41 @@ class TransactionList extends StatelessWidget {
       : ListView.builder (
 				itemBuilder: (ctx, idx) {
 					return Card (
-						child: Row (children: <Widget>[
-							Container (
-								margin: EdgeInsets.symmetric (vertical: 10, horizontal: 15),
-								decoration: BoxDecoration (
-									border: Border.all (
-										color: Theme.of(context).primaryColor,
-										width: 2
-									)
-									),
-								padding: EdgeInsets.all(10),
-								child: Text (
-									'\$${transactions[idx].amount.toStringAsFixed (2)}', 
-									style: TextStyle (
-										fontWeight: FontWeight.bold,
-										fontSize: 20,
-										color: Theme.of(context).primaryColor
-									)
-									)
-								),
-							Column (
-								crossAxisAlignment: CrossAxisAlignment.start,
-								children: <Widget>[
-									Text (
-										transactions[idx].title,
-										// style: TextStyle (
-										// 	fontSize: 18,
-										// 	fontWeight: FontWeight.bold
-										// ),
-                    style: Theme.of(context).textTheme.title,
-										),
-									Text (
-										DateFormat ().format (transactions[idx].date),
-										style: TextStyle (
-											color: Colors.grey
-										),
-										)
-							],)
-						],)
-					);
+            elevation: 5,
+            margin: EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 5
+            ),
+						child: ListTile (
+              leading: Text (
+                '\$${transactions[idx].amount.toStringAsFixed (2)}', 
+                style: TextStyle (
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor
+                )
+              ),
+              title: Text (
+                transactions[idx].title,
+                // style: TextStyle (
+                // 	fontSize: 18,
+                // 	fontWeight: FontWeight.bold
+                // ),
+                style: Theme.of(context).textTheme.title,
+              ),
+              subtitle: Text (
+                DateFormat ().format (transactions[idx].date),
+                style: TextStyle (
+                  color: Colors.grey
+                ),
+              ),
+              trailing: IconButton (
+                icon: Icon (Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () {},
+              ),
+            )
+          );
 				},
 				itemCount: transactions.length,
 			)
