e508fafaa4c7dd4014939b0bf8f501526e4000a8
ermiry <erick_salas@ermiry.com>
2020-02-14 19:55:59 -0600

    Added dedicated colors file

---
+++ lib/style/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color mainPurple = const Color.fromRGBO(49, 39, 79, 1);
+const Color mainLightPurple = const Color.fromRGBO(102, 51, 153, 0.9);
+
+const Color accountFirstColor = const Color(0XFF5F6368);
+// const Color accountFirstColor = const Color.fromRGBO(49, 39, 79, 1);
+const Color accountSecondColor = const Color(0XFFE93B2D);
