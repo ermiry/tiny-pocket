66c7cce687162bdb3cba28e415fef4d66da1de69
ermiry <erick_salas@ermiry.com>
2020-02-14 21:15:52 -0600

    Refactor main.dart to use providers

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
+import 'package:provider/provider.dart';
+import 'package:pocket/providers/auth.dart';
+
 import 'package:pocket/screens/auth.dart';
 
 void main() => runApp(new TinyPocket ());
@@ -10,47 +13,60 @@ void main() => runApp(new TinyPocket ());
 class TinyPocket extends StatelessWidget {
 
 	@override
-	Widget build (BuildContext context) {
+	Widget build(BuildContext context) {
 
-		return Platform.isAndroid ? MaterialApp (
-			title: 'Tiny Pocket',
-      theme: ThemeData (
-        primarySwatch: Colors.blue,
-        accentColor: Colors.redAccent,
-        fontFamily: 'Quicksand',
-        appBarTheme: AppBarTheme (
-          textTheme: ThemeData.light().textTheme.copyWith(
-            title: TextStyle (fontFamily: 'Open Sans', fontSize: 20, fontWeight: FontWeight.bold)
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: new Auth(),
+        )
+      ],
+      child: Consumer <Auth> (
+        builder: (ctx, auth, _) => Platform.isAndroid ? MaterialApp (
+          title: 'Tiny Pocket',
+          theme: ThemeData (
+            // primarySwatch: mainBlue,
+            // accentColor: mainDarkBlue,
+            fontFamily: 'Quicksand',
+            appBarTheme: AppBarTheme (
+              textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle (fontFamily: 'Open Sans', fontSize: 20, fontWeight: FontWeight.bold)
+                )
+            ),
+            textTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle (
+                fontFamily: 'Quicksand',
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+              ),
+              button: TextStyle (
+                color: Colors.white
+              )
             )
-        ),
-        textTheme: ThemeData.light().textTheme.copyWith(
-          title: TextStyle (
-            fontFamily: 'Quicksand',
-            fontSize: 18,
-            fontWeight: FontWeight.bold
           ),
-          button: TextStyle (
-            color: Colors.white
-          )
+          home: new AuthScreen (),
+          debugShowCheckedModeBanner: true,
+        )
+
+        :
+
+        CupertinoApp (
+          title: 'Tiny Pocket',
+          theme: CupertinoThemeData (
+            // primaryColor: Colors.blue,
+            // primaryContrastingColor: Colors.redAccent,
+            textTheme: CupertinoTextThemeData (
+              textStyle: TextStyle (
+                fontFamily: 'Quicksand',
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+              )
+            )
+          ),
+          // home: HomePage ()
+          home: new AuthScreen ()
         )
       ),
-			home: new AuthScreen ()
-		) :
-    CupertinoApp (
-      title: 'Tiny Pocket',
-      theme: CupertinoThemeData (
-        primaryColor: Colors.blue,
-        primaryContrastingColor: Colors.redAccent,
-        textTheme: CupertinoTextThemeData (
-          textStyle: TextStyle (
-            fontFamily: 'Quicksand',
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          )
-        )
-      ),
-			// home: HomePage ()
-			home: new AuthScreen ()
     );
 
 	}
