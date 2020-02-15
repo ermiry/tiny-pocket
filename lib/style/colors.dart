2cb84f432120ee83f8bdabef21ece80d2292a7b6
ermiry <erick_salas@ermiry.com>
2020-02-14 21:31:22 -0600

    Work in overall style

--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const Color mainBlue = const Color.fromRGBO(2, 74, 117, 1);
+const Color mainDarkBlue = const Color.fromRGBO(25, 42, 86, 1);
+
 const Color mainPurple = const Color.fromRGBO(49, 39, 79, 1);
 const Color mainLightPurple = const Color.fromRGBO(102, 51, 153, 0.9);
 
---
+++ lib/style/style.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:pocket/style/colors.dart';
+
+const TextStyle hoursPlayedLabelTextStyle = const TextStyle(
+  fontSize: 16,
+  color: accountFirstColor,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle hoursPlayedTextStyle = const TextStyle(
+  fontSize: 28,
+  color: mainPurple,
+  fontWeight: FontWeight.w800,
+);
+
+const TextStyle infoTitleTextStyle = const TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+  color: mainPurple
+);
+
+const TextStyle infoLabelTextStyle = const TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+  color: mainPurple
+);
+
+const TextStyle infoTextStyle = const TextStyle(
+  fontSize: 16,
+);
