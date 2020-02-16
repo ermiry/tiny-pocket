e0d6df685813352c18d3af3bfc6930f6fe7af373
ermiry <erick_salas@ermiry.com>
2020-02-15 23:22:54 -0600

    Refactor settings page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -2,14 +2,41 @@ import 'package:flutter/material.dart';
 
 import 'package:pocket/sidebar/navigation_bloc.dart';
 
+import 'package:pocket/style/colors.dart';
+
 class SettingsPage extends StatelessWidget with NavigationStates {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Settings",
-        style: TextStyle(fontWeight: FontWeight.w900, fontSize: 28),
+    return Container(
+      // padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Column(
+        children: <Widget>[
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+
+              Center(
+                child: Text(
+                  "Settings",
+                  style: const TextStyle(
+                    fontSize: 32,
+                    color: mainBlue,
+                    fontWeight: FontWeight.w800
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+
+            ],
+          ),
+
+          // 12/02/2020 -- added to fill remaining screen and to avoid bug with sidebar
+          new Expanded (child: Container (),)
+        ],
       ),
     );
   }
