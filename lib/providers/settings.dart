499a6d4af835b045ae6c16e74984f56ac1e2740c
ermiry <erick_salas@ermiry.com>
2020-02-16 18:20:17 -0600

    Added methods to save and load settings

--- lib/providers/settings.dart
+++ lib/providers/settings.dart
@@ -1,24 +1,58 @@
 import 'package:flutter/widgets.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 class Settings with ChangeNotifier {
 
   bool showBarsChart = true;
   bool showExpensesChart = true;
   bool showHistoryChart = false;
 
-  void toggleBarsChart() { 
+  Future <void> toggleBarsChart() async { 
     this.showBarsChart = !this.showBarsChart;
+
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('show_bars_chart', this.showBarsChart);
+
     notifyListeners(); 
   }
 
-  void toggleExpensesChart() { 
+  Future <void> toggleExpensesChart() async { 
     this.showExpensesChart = !this.showExpensesChart;
+
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('show_expenses_chart', this.showExpensesChart);
+
     notifyListeners(); 
   }
 
-  void toggleHistoryChart() { 
+  Future <void> toggleHistoryChart() async { 
     this.showHistoryChart = !this.showHistoryChart;
+
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('show_history_chart', this.showHistoryChart);
+
     notifyListeners(); 
+  }
+
+  Future <void> loadSettings() async {
+
+    final prefs = await SharedPreferences.getInstance();
+
+    if (!prefs.containsKey('show_bars_chart')) {
+      this.showBarsChart = prefs.getBool('show_bars_chart');
+    }
+
+    if (!prefs.containsKey('show_expenses_chart')) {
+      this.showExpensesChart = prefs.getBool('show_expenses_chart');
+    }
+
+    if (!prefs.containsKey('show_history_chart')) {
+      this.showHistoryChart = prefs.getBool('show_history_chart');
+    }
+
+    notifyListeners();
+
   }
 
 }
