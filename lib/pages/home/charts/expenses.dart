9d4cdb6c4363ad518a7f5cc33bae7b2905203802
ermiry <erick_salas@ermiry.com>
2020-02-16 18:51:23 -0600

    Updating expenses chart values based on transactions provider

--- lib/pages/home/charts/expenses.dart
+++ lib/pages/home/charts/expenses.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
+import 'package:provider/provider.dart';
+import 'package:pocket/providers/transactions.dart';
+
 import 'package:fl_chart/fl_chart.dart';
 
 class Indicator extends StatelessWidget {
@@ -61,50 +64,21 @@ class _ExpensesChartState extends State <ExpensesChart> {
   int touchedIndex;
 
   List <PieChartSectionData> showingSections() {
-    return List.generate(4, (i) {
+    return List.generate(Provider.of<Transactions>(context, listen: false).transTypes.length, (i) {
       final isTouched = i == touchedIndex;
       final double fontSize = isTouched ? 25 : 16;
       final double radius = isTouched ? 60 : 50;
-      switch (i) {
-        case 0:
-          return PieChartSectionData(
-            color: const Color(0xff0293ee),
-            value: 40,
-            title: '40%',
-            radius: radius,
-            titleStyle: TextStyle(
-                fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
-          );
-        case 1:
-          return PieChartSectionData(
-            color: const Color(0xfff8b250),
-            value: 30,
-            title: '30%',
-            radius: radius,
-            titleStyle: TextStyle(
-                fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
-          );
-        case 2:
-          return PieChartSectionData(
-            color: const Color(0xff845bef),
-            value: 15,
-            title: '15%',
-            radius: radius,
-            titleStyle: TextStyle(
-                fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
-          );
-        case 3:
-          return PieChartSectionData(
-            color: const Color(0xff13d38e),
-            value: 15,
-            title: '15%',
-            radius: radius,
-            titleStyle: TextStyle(
-                fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
-          );
-        default:
-          return null;
-      }
+      
+      TransactionType transType = Provider.of<Transactions>(context, listen: false).transTypes.elementAt(i);
+      double value = Provider.of<Transactions>(context, listen: false).getTransTypePercentage(transType.type);
+
+      return PieChartSectionData(
+        color: transType.color,
+        value: value,
+        title: '${value.toStringAsFixed (2)}%',
+        radius: radius,
+        titleStyle: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
+      );
     });
   }
 
