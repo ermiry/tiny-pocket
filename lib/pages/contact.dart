8d9d2e74170021e5677bd020643cf018cecf3d2c
ermiry <erick_salas@ermiry.com>
2020-02-22 23:19:00 -0600

    Refactor base contact page layout

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -2,11 +2,41 @@ import 'package:flutter/material.dart';
 
 import 'package:pocket/sidebar/navigation_bloc.dart';
 
+import 'package:pocket/style/colors.dart';
+
 class ContactPage extends StatelessWidget with NavigationStates {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Center(child: Text('Contact'),),);
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: <Widget>[
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+
+              Center(
+                child: Text(
+                  "Contact",
+                  style: const TextStyle(
+                    fontSize: 32,
+                    color: mainBlue,
+                    fontWeight: FontWeight.w800
+                  ),
+                ),
+              ),
+
+              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+            ],
+          ),
+
+          // 12/02/2020 -- added to fill remaining screen and to avoid bug with sidebar
+          new Expanded (child: Container (),),
+        ],
+      ),
+    );
   }
 
 }
