1c8a730f4c343cb4ff8e9190f9233565c62342ac
ermiry <erick_salas@ermiry.com>
2020-02-27 17:10:12 -0600

    Added custom widgets

---
+++ lib/widgets/custom/modal_action_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:pocket/widgets/custom/button.dart';
+
+import 'package:pocket/style/colors.dart';
+
+class CustomModalActionButton extends StatelessWidget {
+
+  final VoidCallback onClose;
+  final VoidCallback onSave;
+
+  CustomModalActionButton({@required this.onClose, @required this.onSave});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        CustomButton(
+          onPressed: onClose,
+          buttonText: "Close",
+        ),
+        CustomButton(
+          onPressed: onSave,
+          buttonText: "Save",
+          color: mainBlue,
+          textColor: Colors.white,
+        )
+      ],
+    );
+  }
+
+}
