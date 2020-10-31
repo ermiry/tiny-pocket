725e5d328d734b1b792dd2227295fc5799f962ac
ermiry <erick_salas@ermiry.com>
2020-10-30 20:05:28 -0600

    Updated main configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,39 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:provider/provider.dart';
 import 'package:pocket/providers/global.dart';
+import 'package:pocket/providers/ui.dart';
 import 'package:pocket/providers/auth.dart';
 import 'package:pocket/providers/transactions.dart';
 import 'package:pocket/providers/settings.dart';
 
 import 'package:pocket/screens/splash.dart';
 import 'package:pocket/screens/welcome.dart';
-import 'package:pocket/screens/auth.dart';
 import 'package:pocket/screens/loading.dart';
-import 'package:pocket/sidebar/sidebar_layout.dart';
+import 'package:pocket/screens/auth/login.dart';
+import 'package:pocket/screens/auth/register.dart';
+import 'package:pocket/screens/drawer.dart';
+import 'package:pocket/screens/active.dart';
 
 import 'package:pocket/style/colors.dart';
 
 void main() => runApp(new TinyPocket ());
+
+class MainScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          DrawerScreen(),
+          ActiveScreen(),
+        ],
+      ),
+    );
+  }
+}
 
 class TinyPocket extends StatelessWidget {
 
@@ -26,77 +42,55 @@ class TinyPocket extends StatelessWidget {
 
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider.value(
-          value: new Global(),
-        ),
-        ChangeNotifierProvider.value(
-          value: new Auth(),
-        ),
-        ChangeNotifierProvider.value(
-          value: new Transactions(),
-        ),
-        ChangeNotifierProvider.value(
-          value: new Settings(),
-        )
+        ChangeNotifierProvider.value(value: new Global()),
+        ChangeNotifierProvider.value(value: new UI()),
+        ChangeNotifierProvider.value(value: new Auth()),
+        ChangeNotifierProvider.value(value: new Transactions()),
+        ChangeNotifierProvider.value(value: new Settings())
       ],
-      child: Platform.isAndroid ? MaterialApp (
-        title: 'Tiny Pocket',
-        theme: ThemeData (
-          primaryColor: mainBlue,
-          fontFamily: 'Quicksand',
-          appBarTheme: AppBarTheme (
-            textTheme: ThemeData.light().textTheme.copyWith(
-              title: TextStyle (fontFamily: 'Open Sans', fontSize: 20, fontWeight: FontWeight.bold)
-              )
-          ),
-          textTheme: ThemeData.light().textTheme.copyWith(
-            title: TextStyle (
-              fontFamily: 'Quicksand',
-              fontSize: 18,
-              fontWeight: FontWeight.bold
+      child: Consumer <Global> (
+        builder: (ctx, global, _) => MaterialApp (
+          title: 'Tiny Pocket',
+          theme: ThemeData (
+            primaryColor: mainBlue,
+            fontFamily: 'Quicksand',
+            appBarTheme: AppBarTheme (
+              textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle (fontFamily: 'Open Sans', fontSize: 20, fontWeight: FontWeight.bold)
+                )
             ),
-            button: TextStyle (
-              color: Colors.white
-            )
-          )
-        ),
-        initialRoute: '/splash',
-        home: Consumer <Auth> (
-          builder: (ctx, auth, _) => Provider.of<Global>(ctx, listen: false).firstTime ? new WelcomeScreen() 
-            :
-            auth.isAuth ? new SideBarLayout () :
-              FutureBuilder(
-                future: auth.tryAutoLogin(),
-                builder: (ctx, authResultSnapshot) =>
-                  authResultSnapshot.connectionState == ConnectionState.waiting ?
-                    new LoadingScreen () : new AuthScreen (),
+            textTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle (
+                fontFamily: 'Quicksand',
+                fontSize: 18,
+                fontWeight: FontWeight.bold
               ),
-        ),
-        routes: {
-          '/splash': (ctx) => new SplashScreen (),
-        },
-
-        debugShowCheckedModeBanner: true,
-      )
-
-      :
-
-      CupertinoApp (
-        title: 'Tiny Pocket',
-        theme: CupertinoThemeData (
-          // primaryColor: Colors.blue,
-          // primaryContrastingColor: Colors.redAccent,
-          textTheme: CupertinoTextThemeData (
-            textStyle: TextStyle (
-              fontFamily: 'Quicksand',
-              fontSize: 18,
-              fontWeight: FontWeight.bold
+              button: TextStyle (
+                color: Colors.white
+              )
             )
-          )
-        ),
-        // home: HomePage ()
-        home: new AuthScreen ()
-      ),
+          ),
+          initialRoute: '/splash',
+          home: Consumer <Auth> (
+            builder: (ctx, auth, _) => global.firstTime ? new WelcomeScreen() 
+              :
+              auth.isAuth ? new MainScreen () :
+                FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) =>
+                    authResultSnapshot.connectionState == ConnectionState.waiting ?
+                      new LoadingScreen () : new LoginScreen (),
+                ),
+          ),
+          routes: {
+            '/splash': (ctx) => new SplashScreen (),
+
+            '/register': (ctx) => new RegisterScreen (),
+          },
+
+          debugShowCheckedModeBanner: true,
+        )
+      )
     );
 
 	}
