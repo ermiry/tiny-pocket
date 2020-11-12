bfbb7026cb6b632104d70c5fd5fe849cf5e78ebf
ermiry <erick_salas@ermiry.com>
2020-11-12 09:17:18 -0600

    Refactored account screen layout

--- lib/screens/account.dart
+++ lib/screens/account.dart
@@ -1,9 +1,7 @@
-import 'package:intl/intl.dart';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:provider/provider.dart';
+import 'package:pocket/providers/ui.dart';
 import 'package:pocket/providers/auth.dart';
 
 import 'package:pocket/widgets/change_value.dart';
@@ -246,203 +244,230 @@ class _AccountScreenState extends State <AccountScreen> {
     );
   }
 
+  Widget _body() {
+    return Consumer <Auth> (
+      builder: (ctx, auth, _) => new Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text ('General', style: new TextStyle(fontSize: 18, color: mainBlue, fontWeight: FontWeight.bold)),
+            ),
+
+            const SizedBox(height: 10),
+
+            Container(
+              // padding: const EdgeInsets.symmetric(horizontal: 12),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(
+                  width: 0.7,
+                  color: mainDarkBlue.withAlpha(204)
+                ))
+              ),
+              child: ListTile(
+                dense: true,
+                title: Text ('Avatar', style: new TextStyle(fontSize: 16)),
+                subtitle: Text('Change your avatar', style: new TextStyle(fontSize: 14),),
+              ),
+            ),
+
+            Container(
+              // padding: const EdgeInsets.symmetric(horizontal: 12),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(
+                  width: 0.7,
+                  color: mainDarkBlue.withAlpha(204)
+                ))
+              ),
+              child: ListTile(
+                dense: true,
+                title: Text ('Name', style: new TextStyle(fontSize: 16)),
+                subtitle: Text(
+                  "${auth.userValues['name']}", 
+                  style: new TextStyle(fontSize: 14)
+                ),
+                onTap: () => this._showNameChangeDialog()
+              ),
+            ),
+
+            Container(
+              // padding: const EdgeInsets.symmetric(horizontal: 12),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(
+                  width: 0.7,
+                  color: mainDarkBlue.withAlpha(204)
+                ))
+              ),
+              child: ListTile(
+                dense: true,
+                title: Text ('Username', style: new TextStyle(fontSize: 16)),
+                subtitle: Text(
+                  "${auth.userValues['username']}", 
+                  style: new TextStyle(fontSize: 14),
+                ),
+              ),
+            ),
+
+            Container(
+              // padding: const EdgeInsets.symmetric(horizontal: 12),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(
+                  width: 0.7,
+                  color: mainDarkBlue.withAlpha(204)
+                ))
+              ),
+              child: ListTile(
+                dense: true,
+                title: Text ('Email', style: new TextStyle(fontSize: 16)),
+                subtitle: Text(
+                  "${auth.userValues['email']}",
+                  style: new TextStyle(fontSize: 14)
+                ),
+                // onTap: () => this._showChangeDialog("Change email", "Enter your email", null),
+              ),
+            ),
+
+            Container(
+              // padding: const EdgeInsets.symmetric(horizontal: 12),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(
+                  width: 0.7,
+                  color: mainDarkBlue.withAlpha(204)
+                ))
+              ),
+              child: ListTile(
+                dense: true,
+                title: Text ('Password', style: new TextStyle(fontSize: 16)),
+                subtitle: Text('Change your password', style: new TextStyle(fontSize: 14),),
+                onTap: () => this._showPasswordChangeDialog(),
+              ),
+            ),
+
+            // FIXME:
+            // Container(
+            //   // padding: const EdgeInsets.symmetric(horizontal: 12),
+            //   decoration: BoxDecoration(
+            //     border: Border(bottom: BorderSide(
+            //       width: 0.7,
+            //       color: mainDarkBlue.withAlpha(204)
+            //     ))
+            //   ),
+            //   child: ListTile(
+            //     dense: true,
+            //     title: Text ('Member Since', style: new TextStyle(fontSize: 16)),
+            //     subtitle: Text(
+            //       DateFormat('dd/MM/yyyy - HH:mm').format(DateTime.parse(auth.userValues['memberSince']).toLocal()),
+            //       style: new TextStyle(fontSize: 14)
+            //     ),
+            //   ),
+            // ),
+
+            new SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text ('Danger Zone', style: new TextStyle(fontSize: 18, color: Colors.red, fontWeight: FontWeight.bold)),
+            ),
+
+            const SizedBox(height: 20),
+
+            Container(
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(
+                  width: 0.7,
+                ))
+              ),
+              child: ListTile(
+                contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                leading: Icon(Icons.exit_to_app, color: Colors.black87,),
+                title: Text ('Logout', style: new TextStyle(fontSize: 16)),
+                onTap: () => this._showConfirmDialog(
+                  'Are you sure you want to logout?', 
+                  true,
+                  Provider.of<Auth>(context, listen: false).logout
+                )
+              ),
+            ),
+
+            Container(
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(
+                  width: 0.7,
+                  color: Colors.red.withAlpha(204)
+                ))
+              ),
+              child: ListTile(
+                contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                leading: Icon(Icons.report_problem, color: Colors.red),
+                title: Text ('Delete account', style: new TextStyle(fontSize: 16, color: Colors.red)),
+                subtitle: const Text('Deleting your account is permanent. All your data will be wiped out immediately and you won\'t be able to get it back.'),
+                onTap: () => this._showConfirmDialog(
+                  'Are you sure you want to delete your account?',
+                  false, 
+                  Provider.of<Auth>(context, listen: false).delete
+                )
+              ),
+            ),
+
+            new SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+          ],
+        ),
+      ),
+    );
+  }
+
 	@override
 	Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      // DeviceOrientation.portraitDown,
-    ]);
-
     return Stack(
       children: <Widget>[
-        SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: ListView(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
 
-              Center(
-                child: Text(
-                  "Account",
-                  style: const TextStyle(
-                    fontSize: 24,
-                    color: mainBlue,
-                    fontWeight: FontWeight.w800
-                  ),
+              Consumer <UI> (
+                builder: (ctx, ui, _) => Row (
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(width: 16.0),
+
+                    ui.isDrawerOpen ? IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      onPressed: null,
+                    )
+
+                    :
+
+                    IconButton(
+                      icon: Icon(Icons.menu),
+                      onPressed: () {
+                        if (!ui.isDrawerOpen) {
+                          ui.openDrawer();
+                        }
+                      }
+                    ),
+
+                    const SizedBox(width: 20.0),
+
+                    Text(
+                      'Account',
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF2F3446)
+                      ),
+                    ),
+                  ],
                 ),
               ),
 
-              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
 
-              Consumer <Auth> (
-                builder: (ctx, auth, _) => new Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 24),
-                        child: Text ('General', style: new TextStyle(fontSize: 18, color: mainBlue, fontWeight: FontWeight.bold)),
-                      ),
-
-                      const SizedBox(height: 10),
-
-                      Container(
-                        // padding: const EdgeInsets.symmetric(horizontal: 12),
-                        decoration: BoxDecoration(
-                          border: Border(bottom: BorderSide(
-                            width: 0.7,
-                            color: mainDarkBlue.withAlpha(204)
-                          ))
-                        ),
-                        child: ListTile(
-                          dense: true,
-                          title: Text ('Avatar', style: new TextStyle(fontSize: 16)),
-                          subtitle: Text('Change your avatar', style: new TextStyle(fontSize: 14),),
-                        ),
-                      ),
-
-                      Container(
-                        // padding: const EdgeInsets.symmetric(horizontal: 12),
-                        decoration: BoxDecoration(
-                          border: Border(bottom: BorderSide(
-                            width: 0.7,
-                            color: mainDarkBlue.withAlpha(204)
-                          ))
-                        ),
-                        child: ListTile(
-                          dense: true,
-                          title: Text ('Name', style: new TextStyle(fontSize: 16)),
-                          subtitle: Text(
-                            "${auth.userValues['name']}", 
-                            style: new TextStyle(fontSize: 14)
-                          ),
-                          onTap: () => this._showNameChangeDialog()
-                        ),
-                      ),
-
-                      Container(
-                        // padding: const EdgeInsets.symmetric(horizontal: 12),
-                        decoration: BoxDecoration(
-                          border: Border(bottom: BorderSide(
-                            width: 0.7,
-                            color: mainDarkBlue.withAlpha(204)
-                          ))
-                        ),
-                        child: ListTile(
-                          dense: true,
-                          title: Text ('Username', style: new TextStyle(fontSize: 16)),
-                          subtitle: Text(
-                            "${auth.userValues['username']}", 
-                            style: new TextStyle(fontSize: 14),
-                          ),
-                        ),
-                      ),
-
-                      Container(
-                        // padding: const EdgeInsets.symmetric(horizontal: 12),
-                        decoration: BoxDecoration(
-                          border: Border(bottom: BorderSide(
-                            width: 0.7,
-                            color: mainDarkBlue.withAlpha(204)
-                          ))
-                        ),
-                        child: ListTile(
-                          dense: true,
-                          title: Text ('Email', style: new TextStyle(fontSize: 16)),
-                          subtitle: Text(
-                            "${auth.userValues['email']}",
-                            style: new TextStyle(fontSize: 14)
-                          ),
-                          // onTap: () => this._showChangeDialog("Change email", "Enter your email", null),
-                        ),
-                      ),
-
-                      Container(
-                        // padding: const EdgeInsets.symmetric(horizontal: 12),
-                        decoration: BoxDecoration(
-                          border: Border(bottom: BorderSide(
-                            width: 0.7,
-                            color: mainDarkBlue.withAlpha(204)
-                          ))
-                        ),
-                        child: ListTile(
-                          dense: true,
-                          title: Text ('Password', style: new TextStyle(fontSize: 16)),
-                          subtitle: Text('Change your password', style: new TextStyle(fontSize: 14),),
-                          onTap: () => this._showPasswordChangeDialog(),
-                        ),
-                      ),
-
-                      Container(
-                        // padding: const EdgeInsets.symmetric(horizontal: 12),
-                        decoration: BoxDecoration(
-                          border: Border(bottom: BorderSide(
-                            width: 0.7,
-                            color: mainDarkBlue.withAlpha(204)
-                          ))
-                        ),
-                        child: ListTile(
-                          dense: true,
-                          title: Text ('Member Since', style: new TextStyle(fontSize: 16)),
-                          subtitle: Text(
-                            DateFormat('dd/MM/yyyy - HH:mm').format(DateTime.parse(auth.userValues['memberSince']).toLocal()),
-                            style: new TextStyle(fontSize: 14)
-                          ),
-                        ),
-                      ),
-
-                      new SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 24),
-                        child: Text ('Danger Zone', style: new TextStyle(fontSize: 18, color: Colors.red, fontWeight: FontWeight.bold)),
-                      ),
-
-                      const SizedBox(height: 20),
-
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border(bottom: BorderSide(
-                            width: 0.7,
-                          ))
-                        ),
-                        child: ListTile(
-                          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                          leading: Icon(Icons.exit_to_app, color: Colors.black87,),
-                          title: Text ('Logout', style: new TextStyle(fontSize: 16)),
-                          onTap: () => this._showConfirmDialog(
-                            'Are you sure you want to logout?', 
-                            true,
-                            Provider.of<Auth>(context, listen: false).logout
-                          )
-                        ),
-                      ),
-
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border(bottom: BorderSide(
-                            width: 0.7,
-                            color: Colors.red.withAlpha(204)
-                          ))
-                        ),
-                        child: ListTile(
-                          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                          leading: Icon(Icons.report_problem, color: Colors.red),
-                          title: Text ('Delete account', style: new TextStyle(fontSize: 16, color: Colors.red)),
-                          subtitle: const Text('Deleting your account is permanent. All your data will be wiped out immediately and you won\'t be able to get it back.'),
-                          onTap: () => this._showConfirmDialog(
-                            'Are you sure you want to delete your account?',
-                            false, 
-                            Provider.of<Auth>(context, listen: false).delete
-                          )
-                        ),
-                      ),
-
-                      new SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-                    ],
-                  ),
-                ),
-              )
+              this._body()
             ],
           ),
         ),
