f779ef16fa0b8d757a84f19ebe8edabd4fbbd92c
ermiry <erick_salas@ermiry.com>
2020-11-12 09:17:35 -0600

    Updated settings screen to work with new drawer & refactored layout

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:provider/provider.dart';
 import 'package:pocket/providers/global.dart';
 import 'package:pocket/providers/settings.dart';
+import 'package:pocket/providers/ui.dart';
 import 'package:pocket/providers/transactions.dart';
 
 import 'package:pocket/style/colors.dart';
@@ -135,211 +135,244 @@ class _SettingsScreenState extends State <SettingsScreen> {
     );
   }
 
-	@override
-	Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      // DeviceOrientation.portraitDown,
-    ]);
-
+  Widget _body() {
     return Consumer <Settings> (
       builder: (ctx, settings, _) {
-        return Container(
-          height: MediaQuery.of(context).size.height,
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text ('General', style: new TextStyle(fontSize: 18, color: mainDarkBlue, fontWeight: FontWeight.bold)),
+              ),
 
-                Center(
-                  child: Text(
-                    "Settings",
-                    style: const TextStyle(
-                      fontSize: 24,
-                      color: mainBlue,
-                      fontWeight: FontWeight.w800
-                    ),
-                  ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 6),
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(
+                    color: mainDarkBlue
+                  ))
                 ),
-
-                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Text ('General', style: new TextStyle(fontSize: 18, color: mainDarkBlue, fontWeight: FontWeight.bold)),
+                child: ListTile(
+                  title: const Text ('Show bars chart'),
+                  subtitle: const Text('Display your last week\'s activity using a bars'),
+                  trailing: Switch.adaptive (
+                    activeColor: mainBlue,
+                    value: settings.showBarsChart,
+                    onChanged: (val) { 
+                      settings.toggleBarsChart();
+                    }
+                  )
                 ),
+              ),
 
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 6),
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide(
-                      color: mainDarkBlue
-                    ))
-                  ),
-                  child: ListTile(
-                    title: const Text ('Show bars chart'),
-                    subtitle: const Text('Display your last week\'s activity using a bars'),
-                    trailing: Switch.adaptive (
-                      activeColor: mainBlue,
-                      value: settings.showBarsChart,
-                      onChanged: (val) { 
-                        settings.toggleBarsChart();
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(
+                    color: mainDarkBlue
+                  ))
+                ),
+                child: ListTile(
+                  title: const Text ('Show expenses chart'),
+                  subtitle: const Text('Display expenses by category pie chart'),
+                  trailing: Switch.adaptive (
+                    activeColor: mainBlue,
+                    value: settings.showExpensesChart,
+                    onChanged: (val) { 
+                      settings.toggleExpensesChart();
+                    }
+                  )
+                ),
+              ),
+
+              // TODO:
+              // Container(
+              //   padding: const EdgeInsets.symmetric(vertical: 5),
+              //   decoration: BoxDecoration(
+              //     border: Border(bottom: BorderSide(
+              //       color: mainDarkBlue
+              //     ))
+              //   ),
+              //   child: ListTile(
+              //     title: const Text ('Show history chart'),
+              //     subtitle: const Text('Display your last week\'s activity using a line chart'),
+              //     trailing: Switch.adaptive (
+              //       activeColor: mainBlue,
+              //       value: settings.showHistoryChart,
+              //       onChanged: (val) { 
+              //         settings.toggleHistoryChart();
+              //       },)
+              //   ),
+              // ),
+
+              new SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(
+                    color: mainDarkBlue
+                  ))
+                ),
+                child: ListTile(
+                  title: const Text ('Center add button'),
+                  subtitle: const Text('Display the add transaction button in the center'),
+                  trailing: Switch.adaptive (
+                    activeColor: mainBlue,
+                    value: settings.centerAddButton,
+                    onChanged: (val) { 
+                      settings.toggleCenterAddButton();
+                    }
+                  )
+                ),
+              ),
+
+              new SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(
+                    color: mainDarkBlue
+                  ))
+                ),
+                child: ListTile(
+                  title: const Text ('Toggle welcome screen'),
+                  subtitle: const Text('Display welcome screen the next time you launch the app'),
+                  trailing: Switch.adaptive (
+                    activeColor: mainBlue,
+                    value: Provider.of<Global>(context).firstTime,
+                    onChanged: (val) async { 
+                      try {
+                        await Provider.of<Global>(context, listen: false).showWelcome();
                       }
-                    )
-                  ),
-                ),
 
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide(
-                      color: mainDarkBlue
-                    ))
-                  ),
-                  child: ListTile(
-                    title: const Text ('Show expenses chart'),
-                    subtitle: const Text('Display expenses by category pie chart'),
-                    trailing: Switch.adaptive (
-                      activeColor: mainBlue,
-                      value: settings.showExpensesChart,
-                      onChanged: (val) { 
-                        settings.toggleExpensesChart();
+                      catch (error) {
+                        print(error);
                       }
-                    )
-                  ),
+                    }
+                  )
                 ),
+              ),
 
-                // TODO:
-                // Container(
-                //   padding: const EdgeInsets.symmetric(vertical: 5),
-                //   decoration: BoxDecoration(
-                //     border: Border(bottom: BorderSide(
-                //       color: mainDarkBlue
-                //     ))
-                //   ),
-                //   child: ListTile(
-                //     title: const Text ('Show history chart'),
-                //     subtitle: const Text('Display your last week\'s activity using a line chart'),
-                //     trailing: Switch.adaptive (
-                //       activeColor: mainBlue,
-                //       value: settings.showHistoryChart,
-                //       onChanged: (val) { 
-                //         settings.toggleHistoryChart();
-                //       },)
-                //   ),
-                // ),
+              new SizedBox(height: MediaQuery.of(context).size.height * 0.1),
 
-                new SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text ('Danger Zone', style: new TextStyle(fontSize: 18, color: Colors.red, fontWeight: FontWeight.bold)),
+              ),
 
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide(
-                      color: mainDarkBlue
-                    ))
-                  ),
-                  child: ListTile(
-                    title: const Text ('Center add button'),
-                    subtitle: const Text('Display the add transaction button in the center'),
-                    trailing: Switch.adaptive (
-                      activeColor: mainBlue,
-                      value: settings.centerAddButton,
-                      onChanged: (val) { 
-                        settings.toggleCenterAddButton();
-                      }
-                    )
-                  ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(
+                    color: mainDarkBlue
+                  ))
                 ),
-
-                new SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide(
-                      color: mainDarkBlue
-                    ))
-                  ),
-                  child: ListTile(
-                    title: const Text ('Toggle welcome screen'),
-                    subtitle: const Text('Display welcome screen the next time you launch the app'),
-                    trailing: Switch.adaptive (
-                      activeColor: mainBlue,
-                      value: Provider.of<Global>(context).firstTime,
-                      onChanged: (val) async { 
-                        try {
-                          await Provider.of<Global>(context, listen: false).showWelcome();
-                        }
-
-                        catch (error) {
-                          print(error);
-                        }
-                      }
-                    )
-                  ),
+                child: ListTile(
+                  title: const Text ('Enable cloud backup'),
+                  subtitle: const Text('Backup your transactions in the cloud'),
+                  trailing: Switch.adaptive (
+                    activeColor: mainBlue,
+                    value: settings.enableCloud,
+                    onChanged: (val) { 
+                      // FIXME:
+                      // settings.toggleHistoryChart();
+                    },)
                 ),
+              ),
 
-                new SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Text ('Danger Zone', style: new TextStyle(fontSize: 18, color: Colors.red, fontWeight: FontWeight.bold)),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(
+                    color: mainDarkBlue
+                  ))
                 ),
-
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide(
-                      color: mainDarkBlue
-                    ))
-                  ),
-                  child: ListTile(
-                    title: const Text ('Enable cloud backup'),
-                    subtitle: const Text('Backup your transactions in the cloud'),
-                    trailing: Switch.adaptive (
-                      activeColor: mainBlue,
-                      value: settings.enableCloud,
-                      onChanged: (val) { 
-                        // FIXME:
-                        // settings.toggleHistoryChart();
-                      },)
-                  ),
+                child: ListTile(
+                  title: Text ('Delete cloud data', style: new TextStyle(color: Colors.red, fontWeight: FontWeight.bold),),
+                  subtitle: const Text('Delete all the transactions and data saved in the cloud'),
                 ),
+              ),
 
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide(
-                      color: mainDarkBlue
-                    ))
-                  ),
-                  child: ListTile(
-                    title: Text ('Delete cloud data', style: new TextStyle(color: Colors.red, fontWeight: FontWeight.bold),),
-                    subtitle: const Text('Delete all the transactions and data saved in the cloud'),
-                  ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(
+                    color: mainDarkBlue
+                  ))
                 ),
-
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide(
-                      color: mainDarkBlue
-                    ))
-                  ),
-                  child: ListTile(
-                    title: Text ('Clear local data', style: new TextStyle(color: Colors.red, fontWeight: FontWeight.bold),),
-                    subtitle: const Text('Clear all the transactions and data saved in this device'),
-                    onTap: () => this._showConfirmDialog(context, "Delete all transactions and data saved on this device?"),
-                  ),
+                child: ListTile(
+                  title: Text ('Clear local data', style: new TextStyle(color: Colors.red, fontWeight: FontWeight.bold),),
+                  subtitle: const Text('Clear all the transactions and data saved in this device'),
+                  onTap: () => this._showConfirmDialog(context, "Delete all transactions and data saved on this device?"),
                 ),
+              ),
 
-                new SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-              ],
-            ),
+              new SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+            ],
           ),
         );
-      },
+      }
+    );
+  }
+
+	@override
+	Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: ListView(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+
+              Consumer <UI> (
+                builder: (ctx, ui, _) => Row (
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(width: 16.0),
+
+                    ui.isDrawerOpen ? IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      onPressed: null,
+                    )
+
+                    :
+
+                    IconButton(
+                      icon: Icon(Icons.menu),
+                      onPressed: () {
+                        if (!ui.isDrawerOpen) {
+                          ui.openDrawer();
+                        }
+                      }
+                    ),
+
+                    const SizedBox(width: 20.0),
+
+                    Text(
+                      'Settings',
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF2F3446)
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+
+              this._body()
+            ],
+          ),
+        )
+      ],
     );
 
 	}
