1100bfd0e0fbf7d4bc6ec385a18d1d3886c24772
ermiry <erick_salas@ermiry.com>
2020-11-17 20:03:57 -0600

    Removed transactions types from transaction provider

--- lib/providers/transactions.dart
+++ lib/providers/transactions.dart
@@ -1,99 +1,12 @@
-import 'dart:convert';
-
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
-
-// import 'package:http/http.dart' as htpp;
-
-// TODO: save transactions as a json value
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// import 'package:pocket/models/http_exception.dart';
-// import 'package:pocket/values.dart';
 
 import 'package:pocket/models/transaction.dart';
 
 import 'package:pref_dessert/pref_dessert.dart';
 
-class TransactionType {
-
-	String id;
-	String title;
-	double amount = 0;
-	Color color;
-
-  int type;
-
-	TransactionType ({
-		@required this.id, 
-		@required this.title, 
-		// @required this.amount, 
-    @required this.color,
-
-    @required this.type
-	});
-
-}
-
 class Transactions with ChangeNotifier {
 
-  // Transactions types:
-  // 0 -> food
-  // 1 -> transportation
-  // 2 -> work
-  // 3 -> fun
-
-  final List <TransactionType> _transTypes = [
-    new TransactionType(id: 'type_1', title: 'Food', color: Color(0xff0293ee), type: 0),
-    new TransactionType(id: 'type_2', title: 'Transportation', color: Color(0xfff8b250), type: 1),
-    new TransactionType(id: 'type_3', title: 'Work', color: Color(0xff845bef), type: 2),
-    new TransactionType(id: 'type_4', title: 'Fun', color: Color(0xff13d38e), type: 3),
-  ];
-
-  List <TransactionType> get transTypes { return [...this._transTypes]; }
-
-  List <TransactionType> get usedTransTypes {
-    return this._transTypes.where((tx) {
-      return tx.amount > 0;
-    }).toList();
-  }
-
-  TransactionType getTransType(int type) {
-    return this._transTypes.firstWhere((t) => t.type == type);
-  }
-
-  double getTransTypePercentage(int type) {
-    double total = 0;
-    double value = 0;
-    this._transactions.forEach((trans) {
-      total += trans.amount;
-      if (trans.type == type) value += trans.amount;
-    });
-
-    return (total == 0 ? 1 : (value * 100) / total);
-  }
-
-  List <DropdownMenuItem <TransactionType>> buildDropdownMenuItems() {
-    List <DropdownMenuItem <TransactionType>> items = List();
-    for (TransactionType trans in this._transTypes) {
-      items.add(
-        DropdownMenuItem(
-          value: trans,
-          child: Text(trans.title),
-        ),
-      );
-    }
-    return items;
-  }
-
-  List <Transaction> _transactions = [
-		// Transaction (id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now (), type: 3),
-		// Transaction (id: 't2', title: 'Weekly Groceries', amount: 20.99, date: DateTime.now (), type: 0),
-    // Transaction (id: 't3', title: 'Dinner', amount: 69.99, date: DateTime.now (), type: 0),
-		// Transaction (id: 't4', title: 'Metro', amount: 20.99, date: DateTime.now (), type: 1),
-    // Transaction (id: 't5', title: 'New work book', amount: 69.99, date: DateTime.now (), type: 2),
-		// Transaction (id: 't6', title: 'Icecream', amount: 20.99, date: DateTime.now (), type: 0)
-	];
+  List <Transaction> _transactions = [];
 
   List <Transaction> get transactions { return [...this._transactions]; }
 
@@ -118,7 +31,7 @@ class Transactions with ChangeNotifier {
     return total;
   }
 
-  Future <void> loadTransactions() async {
+  Future <void> load() async {
 
     // load from local storage
     try {
@@ -126,10 +39,10 @@ class Transactions with ChangeNotifier {
       var list = await repo.findAll();
       this._transactions = list;
 
-      this._transactions.forEach((trans) {
-        TransactionType type = getTransType(trans.type);
-        type.amount += trans.amount;
-      });
+      // this._transactions.forEach((trans) {
+      //   TransactionType type = getTransType(trans.type);
+      //   type.amount += trans.amount;
+      // });
     }
 
     catch (error) {
@@ -140,7 +53,9 @@ class Transactions with ChangeNotifier {
 
   }
 
-  Future <void> addTransaction(String title, double amount, DateTime date, TransactionType type) async {
+  Future <void> add(
+    String title, double amount, DateTime date, String category
+  ) async {
 
     try {
       Transaction trans = Transaction (
@@ -148,18 +63,12 @@ class Transactions with ChangeNotifier {
         title:  title, 
         amount: amount, 
         date: date,
-
-        type: type.type
+        category: category
       );
 
       this._transactions.add(
         trans
       );
-
-      type.amount += amount;
-
-      // String json = jsonEncode(trans);
-      // print(json);
 
       // save to local storage
       var repo = new FuturePreferencesRepository <Transaction> (new TransactionDesSer());
@@ -172,12 +81,9 @@ class Transactions with ChangeNotifier {
 
   }
 
-  void removeTransaction(String id) {
+  void remove(String id) {
 
-    Transaction tx = this._transactions.firstWhere((t) => t.id == id);
-
-    TransactionType type = getTransType(tx.type);
-    type.amount -= tx.amount;
+    this._transactions.firstWhere((t) => t.id == id);
 
     // remove from local storage
     var repo = new FuturePreferencesRepository <Transaction> (new TransactionDesSer());
@@ -189,7 +95,7 @@ class Transactions with ChangeNotifier {
 
   }
 
-  Future <void> clearTransactions() async {
+  Future <void> clear() async {
 
     try {
       var repo = new FuturePreferencesRepository <Transaction> (new TransactionDesSer());
