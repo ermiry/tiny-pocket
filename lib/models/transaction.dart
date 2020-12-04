bc4352e69fba10bdefc179cd7933ef8f63cb1be8
ermiry <erick_salas@ermiry.com>
2020-12-03 19:50:36 -0600

    Added change notifier in transaction model

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
 import 'package:pref_dessert/pref_dessert.dart';
 
-class Transaction {
+class Transaction with ChangeNotifier {
 
 	final String id;
 
