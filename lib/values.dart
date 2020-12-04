a76f537b976b801115826cb46f24bba4798f854b
ermiry <erick_salas@ermiry.com>
2020-12-03 20:11:05 -0600

    Updated to version 0.2

--- lib/values.dart
+++ lib/values.dart
@@ -1,3 +1,3 @@
-// 13/02/2020 -- constant values
+const String serverURL = 'https://pocket.ermiry.com';
 
-const String serverURL = 'http://192.168.100.39';
+// const String serverURL = 'http://192.168.100.39:5000';
--- lib/version.dart
+++ lib/version.dart
@@ -1,2 +1,2 @@
-const String version_number = '1.0';
-const String version_date = '02/April/2020';
+const String version_number = '0.2';
+const String version_date = '03/December/2020';
