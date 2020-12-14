7926a52354249eceb1dd88028e756e3aadae7787
Djcharles26 <jlaraanaya@gmail.com>
2020-12-14 00:19:42 -0600

    Modified trans page in order to edit amount, date and category

--- lib/screens/trans.dart
+++ lib/screens/trans.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:pocket/providers/auth.dart';
+import 'package:pocket/providers/keyboard.dart';
+import 'package:pocket/providers/transactions.dart';
+import 'package:pocket/style/style.dart';
+import 'package:pocket/widgets/adaptive/flatButton.dart';
+import 'package:pocket/widgets/choose_categories.dart';
+import 'package:pocket/widgets/custom/numpad.dart';
 
 import 'package:provider/provider.dart';
 // import 'package:pocket/providers/transactions.dart';
@@ -22,20 +30,103 @@ class TransScreen extends StatefulWidget {
 class _TransScreenState extends State <TransScreen> {
 
   final TextEditingController _titleEditingController = TextEditingController();
+  final TextEditingController _amountEditingController = TextEditingController();
 
   final FocusNode _titleFocusNode = new FocusNode ();
 
   bool _start = true;
   bool _edit = false;
   bool _first = true;
+  bool _numpad = false;
+  bool _loading = false;
+  DateTime _selectedDate = new DateTime.now();
+  String _category = "";
+
+  @override
+  void initState() { 
+    super.initState();
+
+  }
+
+  void unfocusIfNeeded(){
+    if(_titleFocusNode.hasPrimaryFocus){
+      this._titleFocusNode.unfocus();
+    }
+
+    if(this._numpad){
+      this.setState(() {
+        this._numpad = false;
+      });
+    }
+  }
+
+  void _categorySelect() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        // return new AnimatedDialog(
+        //   changeToDialog: true,
+        //   borderRadius: BorderRadius.all(Radius.circular(12)),
+        //   child: new ChooseLabels()
+        // );
+
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12))
+          ),
+          child: new ChooseCategories(this._category)
+        );
+      }
+    ).then((val){
+        print(val);
+      if(val != null){
+        this.setState(() {
+          this._category = val;
+        });
+      }
+    });
+  }
+
 
   @override
   void dispose() {
     this._titleEditingController.dispose();
 
+    this._amountEditingController.dispose();
+
     this._titleFocusNode.dispose();
 
     super.dispose();
+  }
+
+    void _showErrorDialog(String message) {
+    showDialog(
+      context: context, 
+      builder: (ctx) => AlertDialog (
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12.0))
+        ),
+        title: Text (
+          'Error!', 
+          style: const TextStyle(color: Colors.red, fontSize: 28),
+          textAlign: TextAlign.center,
+        ),
+        content: Text (
+          message,
+          style: const TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text ('Okay', style: const TextStyle(color: mainBlue, fontSize: 18, fontWeight: FontWeight.bold)),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      )
+    );
   }
 
   Future <bool> _confirmExit(String description) async {
@@ -96,7 +187,14 @@ class _TransScreenState extends State <TransScreen> {
   Future <bool> _onWillPop() async {
     bool retval = true;
 
-    if (this._titleEditingController.text.isNotEmpty) {
+    if(this._numpad){
+      this.setState(() {
+        this._numpad = false;
+      });
+      return false;
+    }
+
+    if (this._titleEditingController.text.isNotEmpty || this._amountEditingController.text.isNotEmpty || this._category.isNotEmpty) {
       bool value = false;
 
       if (this.widget.baseTrans != null) {
@@ -185,27 +283,39 @@ class _TransScreenState extends State <TransScreen> {
                   // }
 
                   // return to home screen
+                  if(this.widget.baseTrans == null){
+                    print("New");
+                    try{
+                      await Provider.of<Transactions>(context, listen: false).add(
+                        this._titleEditingController.text,
+                        double.parse(this._amountEditingController.text.substring(1)),
+                        this._selectedDate,
+                        this._category,
+                        Provider.of<Auth>(context,listen: false).token
+                      );
+                    }catch(error){
+                      _showErrorDialog(error.toString());
+                    }
+                  }else{
+                    // await Provider.of<Transactions>(context,listen:false).update(
+                    //   this._titleEditingController.text,
+                    //   double.parse(this._amountEditingController.text),
+                    //   this._selectedDate,
+                    //   this._category
+                    // );
+                  }
                   FocusScope.of(context).requestFocus(FocusNode());
                   Navigator.pop(context);
                 },
               ),
             ),
-
-            // clear
-            // IconButton(
-            //   icon: Icon(
-            //     Icons.clear,
-            //   ),
-            //   onPressed: () {
-            //     if (this._textEditingController.text.isNotEmpty) {
-            //       this._textEditingController.clear();
-            //       if (this.widget.baseTrans != null) {
-            //         // this.setState(() { this._edit = true; });
-            //         this._edit = true;
-            //       }
-            //     }
-            //   },
-            // ),
+            IconButton(
+              icon: Icon(
+                Provider.of<Transaction>(context, listen: false).category != null
+                || this._category != "" ? Icons.label : Icons.label_outline),
+              onPressed: this._categorySelect
+            ),
+            
 
             // delete
             IconButton(
@@ -239,6 +349,22 @@ class _TransScreenState extends State <TransScreen> {
     );
   }
 
+  void _chooseDate() {
+    unfocusIfNeeded();
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime (2019),
+      lastDate: DateTime.now()
+    ).then((pickedDate) {
+      if (pickedDate != null) {
+        setState(() {
+          _selectedDate = pickedDate;
+        });
+      } 
+    });
+  }
+
   Widget _body() {
     return Column(
       children: <Widget>[
@@ -268,6 +394,11 @@ class _TransScreenState extends State <TransScreen> {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
           child: TextField(
+            onTap: (){
+              this.setState(() {
+                this._numpad = false;
+              });
+            },
             focusNode: this._titleFocusNode,
             textCapitalization: TextCapitalization.sentences,
             controller: this._titleEditingController,
@@ -299,7 +430,65 @@ class _TransScreenState extends State <TransScreen> {
               }
             },
           ),
-        )
+        ),
+        Consumer<Keyboard>(
+          builder: (context,keyboard, snapshot) {
+            if(!this._start){
+              this._amountEditingController.clear();
+              if(keyboard.value != "")
+                this._amountEditingController.text = "\$" + keyboard.value;
+            }
+            
+
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+              child: TextField(
+                readOnly: true,
+                onTap: () {
+                  this.unfocusIfNeeded();
+
+                  this.setState(() {
+                    this._numpad = true;
+                  });
+                },
+                showCursor: false,
+                textCapitalization: TextCapitalization.sentences,
+                controller: this._amountEditingController,
+                maxLines: null,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                style: TextStyle(
+                  fontSize: 36,
+                  color: mainDarkBlue,
+                  fontWeight: FontWeight.bold
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Amount', border: InputBorder.none
+                ),
+              ),
+            );
+          }
+        ),
+        Container (
+          margin: EdgeInsets.only(top: 24),
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+          child: Row (
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                child: Text (
+                  _selectedDate == null ? 'No date chosen!'
+                  : '${DateFormat.yMMMd().format(_selectedDate)}',
+                  style: _selectedDate == null ? hoursPlayedLabelTextStyle : hoursPlayedTextStyle
+                ),
+              ),
+              AdaptiveFlatButton (
+                'Choose Date', 
+                this._loading ? null : _chooseDate
+              )
+            ],
+          ),
+        ),
       ],
     );
   }
@@ -315,8 +504,11 @@ class _TransScreenState extends State <TransScreen> {
       // var transactions = Provider.of<Transactions>(context, listen: false);
       if (this.widget.baseTrans != null) {
         this._titleEditingController.text = this.widget.baseTrans.title;
-
+        this._amountEditingController.text = this.widget.baseTrans.amount.toString();
+        this._category = this.widget.baseTrans.category;
         // transactions.selectedLabels = this.widget.baseTrans.labels;
+      
+        
       }
 
       // else {
@@ -331,20 +523,47 @@ class _TransScreenState extends State <TransScreen> {
       this.setState(() => this._first = false);
     }
 
-    return WillPopScope(
-      onWillPop: this._onWillPop,
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Consumer <Transaction> (
-          builder: (ctx, trans, _) {
-            return Stack(
-              children: <Widget>[
-                this._body(),
-                this._actions(trans)
-              ],
-            );
-          }
-        )
+    return GestureDetector(
+      onTap: (){
+        unfocusIfNeeded();
+      },
+      child: WillPopScope(
+        onWillPop: this._onWillPop,
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Consumer <Transaction> (
+            builder: (ctx, trans, _) {
+              return Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: this._body()
+                  ),
+                  AnimatedPositioned(
+                    duration: Duration(milliseconds: 150),
+                    bottom: !this._numpad ? MediaQuery.of(context).size.height * -1 : 0,
+                    left: 0,
+                    child: NumPad(),
+                  ),
+                  AnimatedPositioned(
+                    duration: Duration(milliseconds: 150),
+                    curve: Curves.easeIn,
+                    bottom: 
+                      this._numpad 
+                        ?  MediaQuery.of(context).size.height * 0.35
+                        : 0,
+                    left: 0,
+                    right: 0,
+                    child: this._actions(trans)
+                  )
+                ],
+              );
+            }
+          )
+        ),
       ),
     );
   }
